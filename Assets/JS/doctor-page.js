84aa5e2746ead67648a3e7bb5bf0e9e2169ddca3
christopherchinia <chinia214@gmail.com>
2022-05-18 23:13:14 -0400

    on javascript, loading data and declaring variable

--- Assets/JS/doctor-page.js
+++ Assets/JS/doctor-page.js
@@ -1,3 +1,60 @@
-const data[
-    
+const data = [
+{
+    "CONSULTATION" ,
+    "answer":{
+        "a":"Dr.Sarah",
+        "b":"Dr.Michaels",
+        "c":"Dr.Kimberly",
+        "d":"Dr.Christian",
+        "e":"Dr.Samuel",
+        "correct":"a",
+    }
+},
+{
+    "CONSULTATION" ,
+    "answer":{
+        "a":"Dr.Sarah",
+        "b":"Dr.Michaels",
+        "c":"Dr.Kimberly",
+        "d":"Dr.Christian",
+        "e":"Dr.Samuel",
+        "correct":"b",
+    }
+},
+{
+    "CONSULTATION" ,
+    "answer":{
+        "a":"Dr.Sarah",
+        "b":"Dr.Michaels",
+        "c":"Dr.Kimberly",
+        "d":"Dr.Christian",
+        "e":"Dr.Samuel",
+        "correct":"c",
+    }
+},
+{
+    "CONSULTATION" ,
+    "answer":{
+        "a":"Dr.Sarah",
+        "b":"Dr.Michaels",
+        "c":"Dr.Kimberly",
+        "d":"Dr.Christian",
+        "e":"Dr.Samuel",
+        "correct":"d",
+    }
+},
+{
+    "CONSULTATION" ,
+    "answers":{
+        "a":"Dr.Sarah",
+        "b":"Dr.Michaels",
+        "c":"Dr.Kimberly",
+        "d":"Dr.Christian",
+        "e":"Dr.Samuel",
+        "correct":"e",
+    }
+}
 ]
+
+let count = 0;
+let score = 0;
