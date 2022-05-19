6a32d74dfc9bc44f068324d618cb868cda7ec0f0
Mutoi01 <pngando9@gmail.com>
2022-05-19 15:35:51 +0300

    merging with master

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-const email = document.getElementById('#email');
-const error = document.getElementById('error-text');
-const btn = document.getElementById(submit);
+const uname = document.getElementById('uname');
+const pass = document.getElementById('pass');
+const email = document.getElementById('email');
+const form = document.getElementById('form');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  validateInputs();
+});
+
+const validateInputs = () => {
+  const usernameValue = username.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  if (usernameValue === ''){
+    
+  }
+ 
+}
 
 let regExp = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
